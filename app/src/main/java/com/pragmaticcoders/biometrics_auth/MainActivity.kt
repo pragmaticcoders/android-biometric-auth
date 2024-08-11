2a50f6cbb94326c6cd5bbbbe392ac272a10e8862
Jakub Pruszyski <jakub.pruszynski@pragmaticcoders.com>
2024-08-11 15:33:12 +0200

    git init

---
+++ app/src/main/java/com/pragmaticcoders/biometrics_auth/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.pragmaticcoders.biometrics_auth
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.pragmaticcoders.biometrics_auth.ui.theme.Biometrics_authTheme
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Biometrics_authTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Biometrics_authTheme {
+        Greeting("Android")
+    }
+}
